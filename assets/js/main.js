746fd41c3f50fba3134e6cd06862341cfc601c2a
Aleksandr Chesnokov <a.chesnokov.yur.social@gmail.com>
2025-12-05 03:28:19 +0300

    1 - Базовая структура HTML, CSS, JS модулей

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    console.log("LevelMe: Приложение успешно загружено.");
+
+    $('#app-container').html(`
+        <h1 class="app-title">LevelMe</h1>
+        <p>Модуль JavaScript работает. Готовы к верстке!</p>
+    `);
+});
