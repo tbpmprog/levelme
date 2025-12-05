698673f98289164c1098ba8ea3511b82110eb734
Aleksandr Chesnokov <a.chesnokov.yur.social@gmail.com>
2025-12-05 03:48:19 +0300

    2 - Базовая верстка минимального макета (Персонаж, Навыки, Задачи-заглушка)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
     console.log("LevelMe: Приложение успешно загружено.");
-
-    $('#app-container').html(`
-        <h1 class="app-title">LevelMe</h1>
-        <p>Модуль JavaScript работает. Готовы к верстке!</p>
-    `);
 });
